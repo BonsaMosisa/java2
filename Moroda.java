0beacacf14ec8556daadd7e525f40794088d9465
Bonsa Adugna Mosisa <bonsa.adugnam@gmail.com>
2024-04-27 21:10:58 +0300

    my java commit

---
+++ Moroda.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Moroda {
+    public static void main(String args[]){
+        Scanner S=new Scanner(System.in);
+        String name;
+        System.out.println("What is your name");
+        name=S.nextLine();
+        System.out.println(name);
+
+    }
+}
