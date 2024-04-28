8fb3abadcdc473ba4b3431934b4c3127be2055b1
Bonsa Adugna Mosisa <bonsa.adugnam@gmail.com>
2024-04-28 14:01:45 +0300

    Maths Java

--- Maths.java
+++ Maths.java
@@ -6,10 +6,10 @@ public class Maths {
         double number = 25.0;
         double squareRoot = Math.sqrt(number);
        System.out.println(Math.max(12,13));
-       System.out.println("Square root of " + number + " is " + squareRoot);
+       System.out.println("Square root of the number " + number + " is " + squareRoot);
        int Num;
        Scanner Keyb=new Scanner(System.in);
-       System.out.println("PLEASE ENTER YOUR NUMBER :");
+       System.out.println("PLEASE ENTER YOUR NUMBER TO FIND SQORT :");
        Num=Keyb.nextInt();
        System.out.println("The Square root of "+ Num +" is "+ Math.sqrt(Num));
     }
