799dd400b8d9fafb4740ef81f1ac61781dbeb733
Bonsa Adugna Mosisa <bonsa.adugnam@gmail.com>
2024-04-28 13:38:59 +0300

    My Maths Commit

---
+++ Maths.java
@@ -0,0 +1,16 @@
+import java.lang.Math;
+import java.util.Scanner;
+
+public class Maths {
+    public static void main(String[] args) {
+        double number = 25.0;
+        double squareRoot = Math.sqrt(number);
+       System.out.println(Math.max(12,13));
+       System.out.println("Square root of " + number + " is " + squareRoot);
+       int Num;
+       Scanner Keyb=new Scanner(System.in);
+       System.out.println("PLEASE ENTER YOUR NUMBER :");
+       Num=Keyb.nextInt();
+       System.out.println("The Square root of "+ Num +" is "+ Math.sqrt(Num));
+    }
+}
